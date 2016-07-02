0479c8fe83ae531dccf86432a8ad47fd1d8897c3
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-07-02 19:54:58 -0300

    Responsive stuff and some game functions

--- app/index.js
+++ app/index.js
@@ -6,6 +6,28 @@ var app = angular.module('hangmanApp', []);
 app.controller('hangmanController', function ($scope) {
 
     $scope.title = "Angular Hangman";
+    $scope.gameExists = false;
+
+    $scope.startGame = function startGame(playerName){
+        $scope.gameExists = true;
+        $scope.player = {
+            name: playerName,
+            remainingAttempts: 6,
+            lettersTried : [
+            {letter: 'A', enabled: true},
+            {letter: 'B', enabled: true},
+            {letter: 'C', enabled: true}
+            ]
+        };
+    };
+
+    $scope.endGame = function endGame(){
+        $scope.gameExists = false;
+    };
+
+    $scope.guessLetter = function guessLetter(){
+
+    };
 
     $scope.testeParse = function testeParse(word) {
         $scope.parsedWord = "";
---
+++ app/util.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Guilherme on 02/07/2016.
+ */
+
+function generateRandomWord(){
+
+}
+
+function parseWord(){
+
+}
