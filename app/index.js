f9a04ffb99d479b86cc128dc2a56a60a967c8ced
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-07-02 21:20:24 -0300

    'Kind of' responsive keyboard added

--- app/index.js
+++ app/index.js
@@ -6,30 +6,29 @@ var app = angular.module('hangmanApp', []);
 app.controller('hangmanController', function ($scope) {
     $scope.title = "Angular Hangman";
     $scope.showTip = false;
+    $scope.lettersFirstRow = ['A', 'B', 'C', 'D', 'E',
+        'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
+    $scope.lettersSecondRow = ['N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    $scope.startGame = function startGame(playerName){
+    $scope.startGame = function startGame(playerName) {
         $scope.gameExists = true;
         $scope.player = {
             name: playerName,
-            remainingAttempts: 6,
-            lettersTried : [
-            {letter: 'A', enabled: true},
-            {letter: 'B', enabled: true},
-            {letter: 'C', enabled: true}
-            ]
+            remainingAttempts: 6
         };
         $scope.gameWord = generateRandomWord();
     };
 
-    $scope.endGame = function endGame(){
+    $scope.endGame = function endGame() {
         $scope.gameExists = false;
     };
 
-    $scope.guessLetter = function guessLetter(){
+    $scope.guessLetter = function guessLetter() {
 
     };
 
-    $scope.toggleTip = function toggleTip(){
+    $scope.toggleTip = function toggleTip() {
         $scope.showTip = ($scope.showTip) ? false : true;
     };
 });
